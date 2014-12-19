fc279ac23c4fb96de3f1e9a0ad1ec1ce56a4b0dc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-12-19 15:41:41 -0500

    mocking response codes, but not the data itself in response

--- service-turtle.js
+++ service-turtle.js
@@ -9,9 +9,34 @@ self.addEventListener('activate', function (event) {
   console.log(name, 'activated');
 });
 
+var mocks;
+
 self.addEventListener('fetch', function (event) {
   console.log(name, 'fetch', event);
-  if (/some\/url/.test(event.request.url)) {
-    event.respondWith(new Response("Hello world!"));
-  }
+
+  mocks = mocks || {};
+
+  Object.keys(mocks).forEach(function (url) {
+    var urlReg = new RegExp(url);
+    if (urlReg.test(event.request.url)) {
+      var mockData = mocks[url];
+      event.respondWith(new Response(mockData.data, {
+        status: mockData.code,
+        responseText: mockData.data
+      }));
+    }
+  });
+
 });
+
+// use window.navigator.serviceWorker.controller.postMessage('hi')
+// to communicate with this service worker
+self.onmessage = function onMessage(event) {
+  console.log('message to service worker', event.data);
+  if (event.data.url) {
+    console.log('registering mock response for url', event.data.url);
+
+    mocks = mocks || {};
+    mocks[event.data.url] = event.data;
+  }
+};
