bddf3782bad675060c157331673755c0e21ce8e8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-01-17 22:50:24 -0500

    tested javascript file download and changing

---
+++ foo.js
@@ -0,0 +1,10 @@
+// simple javascript file
+function foo() {
+  return 'foo';
+}
+
+function bar() {
+  return 'bar';
+}
+
+console.log(foo());
--- service-turtle.js
+++ service-turtle.js
@@ -12,8 +12,56 @@ self.addEventListener('activate', function () {
 
 var mocks;
 
+var allowJsonFromAnywhere = {
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json; charset=utf-8'
+};
+
+function responseFromOptions(options) {
+  options = options || {};
+
+  var responseOptions = {
+    status: options.code || options.status || options.statusCode,
+    headers: allowJsonFromAnywhere
+  };
+
+  var body = JSON.stringify(options.body || options.data);
+  return new Response(body, responseOptions);
+}
+
+var allowJavaScriptFromAnywhere = {
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'text/javascript; charset=utf-8'
+};
+
+function javascriptResponse(src) {
+  var responseOptions = {
+    status: 200,
+    headers: allowJavaScriptFromAnywhere
+  };
+  return new Response(src, responseOptions);
+}
+
 self.addEventListener('fetch', function (event) {
   console.log(myName, 'fetch', event);
+
+  if (/foo\.js$/.test(event.request.url)) {
+    event.respondWith(
+      self.fetch(event.request.clone())
+        .then(function (response) {
+          // response has possible text / json / blob as promises
+          return response.text().then(function (src) {
+            console.log('original source');
+            console.log(src);
+            // src - original source code
+            var transformedSource = 'console.log("hi there, from ServiceWorker!");';
+            // construct new response with changed JavaScript source
+            return javascriptResponse(transformedSource);
+          });
+        })
+    );
+    return;
+  }
 
   mocks = mocks || {};
 
@@ -23,19 +71,9 @@ self.addEventListener('fetch', function (event) {
       var mockData = mocks[url];
       var options = mockData.options || {};
 
-      var responseOptions = {
-        status: options.code || options.status || options.statusCode,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json; charset=utf-8'
-        }
-      };
-
-      var body = JSON.stringify(options.body || options.data);
-      var response = new Response(body, responseOptions);
+      var response = responseFromOptions(mockData.options);
 
       if (options.timeout) {
-
         event.respondWith(new Promise(function (resolve) {
           setTimeout(function () {
             resolve(response);
